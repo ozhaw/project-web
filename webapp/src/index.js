cefd71bfc2c1c8f70d1933212a9ef80325a6e2f6
Oleksandr <alexzanderzharkov@gmail.com>
2019-11-07 16:52:55 +0200

    Login, Register page

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import {createBrowserHistory} from "history";
+import {Redirect, Route, Router, Switch} from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "assets/scss/paper-dashboard.scss?v=1.1.0";
@@ -9,15 +9,19 @@ import "assets/demo/demo.css";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import AdminLayout from "layouts/Admin.jsx";
+import SignIn from "./views/SignIn";
+import SignUp from "./views/SignUp";
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" render={props => <AdminLayout {...props} />} />
-      <Redirect to="/admin/dashboard" />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
+	<Router history={hist}>
+		<Switch>
+			<Route path="/admin" render={props => <AdminLayout {...props} />}/>
+			<Route path="/login" render={props => <SignIn {...props} />}/>
+			<Route path="/new" render={props => <SignUp {...props} />}/>
+			<Redirect to="/admin/dashboard"/>
+		</Switch>
+	</Router>,
+	document.getElementById("root")
 );
