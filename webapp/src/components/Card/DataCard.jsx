54b5293a6909e2febec1392ab6a07371a18dcfbf
Oleksandr <alexzanderzharkov@gmail.com>
2019-11-13 17:17:08 +0200

    Added devices list to dashboard

--- webapp/src/components/Card/DataCard.jsx
+++ webapp/src/components/Card/DataCard.jsx
@@ -1,40 +1,35 @@
 import React from "react";
 import {Card, CardBody, CardFooter, CardTitle, Col, Row} from "reactstrap";
 
-class DataCard extends React.Component {
+export default function DataCard(props) {
+	const cardIcon = `nc-icon ${props.icon} ${props.iconColor}`;
 
-    render() {
-        const cardIcon = `nc-icon ${this.props.icon} ${this.props.iconColor}`;
-
-        return (
-            <>
-                <Card className="card-stats">
-                    <CardBody>
-                        <Row>
-                            <Col md="4" xs="5">
-                                <div className="icon-big text-center icon-warning">
-                                    <i className={cardIcon}/>
-                                </div>
-                            </Col>
-                            <Col md="8" xs="7">
-                                <div className="numbers">
-                                    <p className="card-category">{this.props.title}</p>
-                                    <CardTitle tag="p">{this.props.value}</CardTitle>
-                                    <p/>
-                                </div>
-                            </Col>
-                        </Row>
-                    </CardBody>
-                    <CardFooter>
-                        <hr/>
-                        <div className="stats">
-                            {this.props.footer}
-                        </div>
-                    </CardFooter>
-                </Card>
-            </>
-        );
-    }
-}
-
-export default DataCard;
+	return (
+		<>
+			<Card className="card-stats">
+				<CardBody>
+					<Row>
+						<Col md="4" xs="5">
+							<div className="icon-big text-center icon-warning">
+								<i className={cardIcon}/>
+							</div>
+						</Col>
+						<Col md="8" xs="7">
+							<div className="numbers">
+								<p className="card-category">{props.title}</p>
+								<CardTitle tag="p">{props.value}</CardTitle>
+								<p/>
+							</div>
+						</Col>
+					</Row>
+				</CardBody>
+				<CardFooter>
+					<hr/>
+					<div className="stats">
+						{props.footer}
+					</div>
+				</CardFooter>
+			</Card>
+		</>
+	);
+};
--- webapp/src/views/Dashboard.jsx
+++ webapp/src/views/Dashboard.jsx
@@ -1,56 +1,122 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Col, Row} from "reactstrap";
-import {
-    dashboard24HoursPerformanceChart,
-    dashboardEmailStatisticsChart,
-    dashboardNASDAQChart
-} from "variables/charts.jsx";
+import {dashboard24HoursPerformanceChart, dashboardNASDAQChart} from "variables/charts.jsx";
 
 import Notifications from './Notifications';
 import DataCard from '../components/Card/DataCard';
 import {allIcons, colors} from "../variables/icons";
 import LinePeakGraphCard from "../components/Graph/LinePeakGraphCard";
 import LineDotGraphCard from "../components/Graph/LineDotGraphCard";
-import LinePieGraphCard from "../components/Graph/LinePieGraphCard";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import Typography from "@material-ui/core/Typography";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {createRESTUrl} from "../variables/general";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
 
-class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.notif = React.createRef();
+const useStyles = makeStyles(theme => ({
+	heading: {
+		fontSize: theme.typography.pxToRem(15),
+		fontWeight: theme.typography.fontWeightRegular,
+		color: 'white'
+	},
+	ePanel: {
+		marginBottom: '15px',
+		background: '#2c2c2c'
+	},
+    loader: {
+        marginBottom: '15px'
     }
+}));
 
-    onClick = () => {
-        this.notif.current.notify("Hello!");
-    };
+export default function Dashboard() {
+	const [devicesData, setDevicesData] = useState(null);
+	const [devicesDataLoaded, setDevicesDataLoaded] = useState(false);
 
-    render() {
-        return (
-            <>
-                <div className="content">
-                    <Row>
-                        <Col lg="3" md="6" sm="6">
-                            <DataCard title={"Capacity"}
-                                      value={"500 GB"}
-                                      footer={"Update"}
-                                      icon={allIcons["nc-basket"].name}
-                                      iconColor={colors.blue}
-                            />
-                        </Col>
-                    </Row>
-                    <LinePeakGraphCard name={"name"}
-                                       category={"category"}
-                                       data={dashboard24HoursPerformanceChart.data}
-                    />
-                    <LineDotGraphCard name={"name"}
-                                      category={"category"}
-                                      data={dashboardNASDAQChart.data}
-                    />
-                </div>
-                <button onClick={this.onClick}>Click</button>
-                <Notifications type={"success"} place={"br"} ref={this.notif}/>
-            </>
-        );
-    }
+	const notif = React.createRef();
+	const classes = useStyles();
+
+	useEffect(() => {
+		if (!devicesDataLoaded) {
+			new Promise((resolve, reject) => {
+				fetch(createRESTUrl(`/device/api/device/users/${sessionStorage.getItem("userId")}/devices`), {
+					method: 'GET',
+					headers: {
+						'Authorization': `Bearer ${sessionStorage.getItem("sessionToken")}`
+					}
+				}).then((response) => {
+					if (response.ok) {
+						resolve(response.json());
+					} else {
+						reject();
+					}
+				});
+			}).then((result) => {
+				setDevicesData(result);
+				setDevicesDataLoaded(true);
+			});
+		}
+	});
+
+	const onClick = () => {
+		notif.current.notify("Hello!");
+	};
+
+	return (
+		<>
+			<div className="content">
+				{devicesDataLoaded ?
+					<ExpansionPanel className={classes.ePanel}>
+						<ExpansionPanelSummary
+							aria-controls="panel1a-content"
+							id="panel1a-header"
+							expandIcon={<ExpandMoreIcon/>}
+						>
+							<Typography className={classes.heading}>User`s Devices</Typography>
+						</ExpansionPanelSummary>
+						<ExpansionPanelDetails>
+							<Row>
+								{devicesData.map((item, index) =>
+									<Col>
+										<DataCard title={item.type}
+										          value={item.deviceId}
+										          footer={"Active"}
+										          icon={allIcons["nc-camera-compact"].name}
+										          iconColor={colors.blue}
+										/>
+									</Col>
+								)}
+							</Row>
+						</ExpansionPanelDetails>
+					</ExpansionPanel> : (
+						<Grid
+                            className={classes.loader}
+							container
+							spacing={0}
+							alignItems="center"
+							justify="center"
+						>
+							<CircularProgress color="secondary" disableShrink/>
+						</Grid>
+					)}
+
+				<LinePeakGraphCard name={"name"}
+				                   category={"category"}
+				                   data={dashboard24HoursPerformanceChart.data}
+				/>
+				<LineDotGraphCard name={"name"}
+				                  category={"category"}
+				                  data={dashboardNASDAQChart.data}
+				/>
+			</div>
+			<button onClick={() => {
+				onClick()
+			}}>Click
+			</button>
+			<Notifications type={"success"} place={"br"} ref={notif}/>
+		</>
+	);
 }
-
-export default Dashboard;
