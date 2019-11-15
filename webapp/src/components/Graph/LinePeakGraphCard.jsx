3bab8c8832d7074f9cf5879e8c5f2f83532c8c91
Oleksandr <alexzanderzharkov@gmail.com>
2019-11-15 15:55:30 +0200

    Added filtering

--- webapp/src/components/Graph/LinePeakGraphCard.jsx
+++ webapp/src/components/Graph/LinePeakGraphCard.jsx
@@ -1,81 +1,159 @@
 import React, {useState} from "react";
 import {
-    Card,
-    CardBody,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-    Col,
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
-    Row
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+	Col,
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownToggle,
+	Row
 } from "reactstrap";
 import {Bar} from "react-chartjs-2";
+import moment from "moment";
+
+const notNegativeAndLess = (value, range) => value >= 0 && value <= range;
 
 export default function LinePeakGraphCard(props) {
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const updateTime = new Date();
-    const options = {
-        legend: {
-            display: false
-        },
+	const [dropdownState, setDropdownState] = useState({
+		dataDropdownOpen: false,
+		timeDropDownOpen: false
+	});
 
-        tooltips: {
-            enabled: true
-        }
-    };
+	const updateTime = new Date();
+	const options = {
+		legend: {
+			display: false
+		},
 
-    const toggle = () => setDropdownOpen(prevState => !prevState);
-    const getLastUpdatedTime = () => Math.round((updateTime - new Date().getTime()) / 60000);
+		tooltips: {
+			enabled: true
+		}
+	};
 
-    return (
-        <>
-            <Row>
-                <Col md="12">
-                    <Card>
-                        <CardHeader>
-                            <Row>
-                                <Col>
-                                    <CardTitle tag="h5">{props.name}</CardTitle>
-                                </Col>
-                                {props.dropdownData && props.dropdownData.name && props.dropdownData.data ?
-                                    <Col>
-                                        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-                                            <DropdownToggle caret>
-                                                {props.dropdownData.name}
-                                            </DropdownToggle>
-                                            <DropdownMenu>
-                                                {props.dropdownData.data.map(item =>
-                                                    <DropdownItem>
-                                                        <div onClick={() => {props.dropdownCallback(item.value)}}> {item.label}</div>
-                                                    </DropdownItem>
-                                                )}
-                                            </DropdownMenu>
-                                        </Dropdown>
-                                    </Col> : null
-                                }
-                            </Row>
-                            <p className="card-category">{props.category}</p>
-                        </CardHeader>
-                        <CardBody>
-                            <Bar
-                                data={props.data}
-                                options={options}
-                                width={400}
-                                height={100}
-                            />
-                        </CardBody>
-                        <CardFooter>
-                            <hr/>
-                            <div className="stats">
-                                <i className="fa fa-history"/> Updated {getLastUpdatedTime()} minutes ago
-                            </div>
-                        </CardFooter>
-                    </Card>
-                </Col>
-            </Row>
-        </>
-    );
+	const dateRanges = [
+		{
+			name: "All time",
+			isValid: (date) => true
+		},
+		{
+			name: "This year",
+			isValid: (date) => moment(new Date()).diff(moment(date), 'years') === 0
+		},
+		{
+			name: "This month",
+			isValid: (date) => moment(new Date()).diff(moment(date), 'months') === 0
+		},
+		{
+			name: "This week",
+			isValid: (date) => moment(new Date()).diff(moment(date), 'weeks') === 0
+		},
+		{
+			name: "This day",
+			isValid: (date) => moment(new Date()).diff(moment(date), 'days') === 0
+		},
+		{
+			name: "This half-day",
+			isValid: (date) => notNegativeAndLess(moment(new Date()).diff(moment(date), 'hours'), 12)
+		},
+		{
+			name: "This hour",
+			isValid: (date) => notNegativeAndLess(moment(new Date()).diff(moment(date), 'hours'), 1)
+		},
+		{
+			name: "30 minutes",
+			isValid: (date) => notNegativeAndLess(moment(new Date()).diff(moment(date), 'minutes'), 30)
+		},
+		{
+			name: "15 minutes",
+			isValid: (date) => notNegativeAndLess(moment(new Date()).diff(moment(date), 'minutes'), 15)
+		}
+	];
+
+	const toggle = () => {
+		let state = {...dropdownState};
+		state.dataDropdownOpen = !state.dataDropdownOpen;
+		setDropdownState(state);
+	};
+
+	const toggleTime = () => {
+		let state = {...dropdownState};
+		state.timeDropDownOpen = !state.timeDropDownOpen;
+		setDropdownState(state);
+	};
+
+
+	const getLastUpdatedTime = () => Math.round((updateTime - new Date().getTime()) / 60000);
+
+	return (
+		<>
+			<Row>
+				<Col md="12">
+					<Card>
+						<CardHeader>
+							<Row>
+								<Col>
+									<CardTitle tag="h5">{props.name}</CardTitle>
+								</Col>
+								{props.dropdownData && props.dropdownData.name && props.dropdownData.data ?
+									<Col>
+										<Dropdown isOpen={dropdownState.dataDropdownOpen} toggle={toggle}>
+											<DropdownToggle caret>
+												{props.dropdownData.name}
+											</DropdownToggle>
+											<DropdownMenu>
+												{props.dropdownData.data.map(item =>
+													<DropdownItem>
+														<div onClick={() => {
+															props.dropdownCallback(item.value)
+														}}>{item.label}</div>
+													</DropdownItem>
+												)}
+											</DropdownMenu>
+										</Dropdown>
+									</Col> : null
+								}
+								{props.enableTimeRange ?
+									<Col>
+										<Dropdown isOpen={dropdownState.timeDropDownOpen} toggle={toggleTime}>
+											<DropdownToggle caret>
+												Time Range
+											</DropdownToggle>
+											<DropdownMenu>
+												{dateRanges.map(item =>
+													<DropdownItem>
+														<div onClick={() => {
+															props.timeCallback(item.isValid)
+														}}>{item.name}</div>
+													</DropdownItem>
+												)}
+											</DropdownMenu>
+										</Dropdown>
+									</Col> : null
+								}
+							</Row>
+							<p className="card-category">{props.category}</p>
+						</CardHeader>
+						<CardBody>
+							<Bar
+								data={props.data}
+								options={options}
+								width={400}
+								height={100}
+							/>
+						</CardBody>
+						<CardFooter>
+							<hr/>
+							<div className="stats">
+								<i className="fa fa-history"/> Updated {getLastUpdatedTime()} minutes ago
+							</div>
+						</CardFooter>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
 }
--- webapp/src/views/Dashboard.jsx
+++ webapp/src/views/Dashboard.jsx
@@ -16,36 +16,67 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles(theme => ({
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        fontWeight: theme.typography.fontWeightRegular,
-        color: 'white'
-    },
-    ePanel: {
-        marginBottom: '15px',
-        background: '#2c2c2c'
-    },
-    loader: {
-        marginBottom: '15px'
-    }
+	heading: {
+		fontSize: theme.typography.pxToRem(15),
+		fontWeight: theme.typography.fontWeightRegular,
+		color: 'white'
+	},
+	ePanel: {
+		marginBottom: '15px',
+		background: '#2c2c2c'
+	},
+	loader: {
+		marginBottom: '15px'
+	}
 }));
 
 export default function Dashboard() {
-    const [localState, setLocalState] = useState({
-        devicesData: null,
-        devicesDataLoaded: false,
-        deviceInfoData: null,
-        deviceInfoDataLoaded: false,
-        selectedDeviceIndex: -1
-    });
+	const [localState, setLocalState] = useState({
+		devicesData: null,
+		devicesDataLoaded: false,
+		deviceInfoData: null,
+		deviceInfoDataLoaded: false,
+		selectedDeviceIndex: -1,
+		filteredDeviceInfoData: null
+	});
 
-    const notif = React.createRef();
-    const classes = useStyles();
+	const notif = React.createRef();
+	const classes = useStyles();
 
-    useEffect(() => {
-        if (!localState.devicesDataLoaded) {
+	useEffect(() => {
+		if (!localState.devicesDataLoaded) {
+			new Promise((resolve, reject) => {
+				fetch(createRESTUrl(`/device/api/device/users/${sessionStorage.getItem("userId")}/devices`), {
+					method: 'GET',
+					headers: {
+						'Authorization': `Bearer ${sessionStorage.getItem("sessionToken")}`
+					}
+				}).then((response) => {
+					if (response.ok) {
+						resolve(response.json());
+					} else {
+						reject();
+					}
+				});
+			}).then((result) => {
+				let state = {...localState};
+				if (result) {
+					state.devicesData = result.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
+                    state.selectedDeviceIndex = state.devicesData[0].id;
+					state.devicesDataLoaded = true;
+
+					setLocalState(state);
+				}
+			});
+		} else if (localState.devicesDataLoaded && !localState.deviceInfoDataLoaded) {
+			return getDeviceDetailedInfo();
+		}
+	});
+
+	const getDeviceDetailedInfo = () => {
+	    if (localState.selectedDeviceIndex !== -1) {
             new Promise((resolve, reject) => {
-                fetch(createRESTUrl(`/device/api/device/users/${sessionStorage.getItem("userId")}/devices`), {
+                fetch(createRESTUrl(`/device/api/device/${localState.selectedDeviceIndex}/info`), {
                     method: 'GET',
                     headers: {
                         'Authorization': `Bearer ${sessionStorage.getItem("sessionToken")}`
@@ -60,141 +91,145 @@ export default function Dashboard() {
             }).then((result) => {
                 let state = {...localState};
                 if (result) {
-                    state.selectedDeviceIndex = 0;
-                    state.devicesData = result.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
-                    state.devicesDataLoaded = true;
+                	result.forEach(item => {
+                		item.auditDate = new Date(item.auditDate)
+	                });
+                	
+                    state.deviceInfoData = result;
+                    state.deviceInfoDataLoaded = true;
+                    state.filteredDeviceInfoData = result;
 
                     setLocalState(state);
                 }
             });
-        } else if (localState.devicesDataLoaded && !localState.deviceInfoDataLoaded) {
-            return getDeviceDetailedInfo();
         }
-    });
+	};
 
-    const getDeviceDetailedInfo = () => {
-        new Promise((resolve, reject) => {
-            fetch(createRESTUrl(`/device/api/device/${localState.devicesData[localState.selectedDeviceIndex].id}/info`), {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${sessionStorage.getItem("sessionToken")}`
-                }
-            }).then((response) => {
-                if (response.ok) {
-                    resolve(response.json());
-                } else {
-                    reject();
-                }
-            });
-        }).then((result) => {
+	const onClick = () => {
+		notif.current.notify("Hello!");
+	};
+
+	const timeRangeCallback = (isValid) => {
+		if (localState.deviceInfoDataLoaded) {
+			let state = {...localState};
+			state.filteredDeviceInfoData = localState.deviceInfoData.filter(item => isValid(item.auditDate));
+			setLocalState(state);
+		}
+	};
+
+	const deviceInfoChartCallback = (id) => {
+	    if (id !== localState.selectedDeviceIndex) {
             let state = {...localState};
-            if (result) {
-                state.deviceInfoData = result;
-                state.deviceInfoDataLoaded = true;
-
-                setLocalState(state);
-            }
-        });
-    };
-
-    const onClick = () => {
-        notif.current.notify("Hello!");
-    };
-
-    const deviceInfoChartCallback = (id) => {
-        alert(id);
-    };
-
-    const prepareDeviceInfoDataTemplate = () => {
-        let combinedData = {};
-        localState.deviceInfoData.forEach(item => {
-            combinedData[item.deviceStatus] = combinedData[item.deviceStatus] ? combinedData[item.deviceStatus] + 1 : 1
-        });
-
-        let labels = [];
-        let datasets = [];
-
-        for (let key of Object.keys(combinedData)) {
-            labels.push(key);
-            datasets.push(combinedData[key]);
+            state.selectedDeviceIndex = id;
+            state.deviceInfoData = null;
+            state.deviceInfoDataLoaded = false;
+            setLocalState(state);
         }
+	};
 
-        let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+	const barColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-        return {
-            labels: labels,
-            datasets: [{
-                borderColor: color,
-                backgroundColor: color,
-                pointRadius: 1,
-                pointHoverRadius: 1,
-                borderWidth: 3,
-                data: datasets,
-                fill: true
-            }]
-        };
-    };
+	const prepareDeviceInfoDataTemplate = () => {
+		let combinedData = {};
+		localState.filteredDeviceInfoData.forEach(item => {
+			combinedData[item.deviceStatus] = combinedData[item.deviceStatus] ? combinedData[item.deviceStatus] + 1 : 1
+		});
 
-    return (
-        <>
-            <div className="content">
-                {localState.devicesDataLoaded ?
-                    <ExpansionPanel className={classes.ePanel}>
-                        <ExpansionPanelSummary
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                            expandIcon={<ExpandMoreIcon/>}
-                        >
-                            <Typography className={classes.heading}>User`s Devices</Typography>
-                        </ExpansionPanelSummary>
-                        <ExpansionPanelDetails>
-                            <Row>
-                                {localState.devicesData.map((item, index) =>
-                                    <Col>
-                                        <DataCard title={item.type}
-                                                  value={item.deviceId}
-                                                  footer={"Active"}
-                                                  icon={allIcons["nc-camera-compact"].name}
-                                                  iconColor={colors.blue}
-                                        />
-                                    </Col>
-                                )}
-                            </Row>
-                        </ExpansionPanelDetails>
-                    </ExpansionPanel> : (
-                        <Grid
-                            className={classes.loader}
-                            container
-                            spacing={0}
-                            alignItems="center"
-                            justify="center"
-                        >
-                            <CircularProgress color="secondary" disableShrink/>
-                        </Grid>
-                    )}
+		let labels = [];
+		let datasets = [];
 
-                {localState.deviceInfoDataLoaded ?
-                    <LinePeakGraphCard name={"Device Information"}
-                                       category={"Device state for a period of time"}
-                                       data={() => prepareDeviceInfoDataTemplate()}
-                    /> : (
-                        <Grid
-                            className={classes.loader}
-                            container
-                            spacing={0}
-                            alignItems="center"
-                            justify="center"
-                        >
-                            <CircularProgress color="secondary" disableShrink/>
-                        </Grid>
-                    )}
+		for (let key of Object.keys(combinedData)) {
+			labels.push(key);
+			datasets.push(combinedData[key]);
+		}
 
-            </div>
-            <button onClick={() => {
-                onClick()
-            }}>Click
-            </button>
-            <Notifications type={"success"} place={"br"} ref={notif}/>
-        </>
-    );
+		return {
+			labels: labels,
+			datasets: [{
+				borderColor: barColor,
+				backgroundColor: barColor,
+				pointRadius: 1,
+				pointHoverRadius: 1,
+				borderWidth: 3,
+				data: datasets,
+				fill: true
+			}]
+		};
+	};
+
+	return (
+		<>
+			<div className="content">
+				{localState.devicesDataLoaded ?
+					<ExpansionPanel className={classes.ePanel}>
+						<ExpansionPanelSummary
+							aria-controls="panel1a-content"
+							id="panel1a-header"
+							expandIcon={<ExpandMoreIcon/>}
+						>
+							<Typography className={classes.heading}>User`s Devices</Typography>
+						</ExpansionPanelSummary>
+						<ExpansionPanelDetails>
+							<Row>
+								{localState.devicesData.map((item, index) =>
+									<Col>
+										<DataCard title={item.type}
+										          value={item.deviceId}
+										          footer={"Active"}
+										          icon={allIcons["nc-camera-compact"].name}
+										          iconColor={colors.blue}
+										/>
+									</Col>
+								)}
+							</Row>
+						</ExpansionPanelDetails>
+					</ExpansionPanel> : (
+						<Grid
+							className={classes.loader}
+							container
+							spacing={0}
+							alignItems="center"
+							justify="center"
+						>
+							<CircularProgress color="secondary" disableShrink/>
+						</Grid>
+					)}
+
+				{localState.deviceInfoDataLoaded ?
+					<LinePeakGraphCard name={"Device Information"}
+					                   category={"Device state for a period of time"}
+					                   data={() => prepareDeviceInfoDataTemplate()}
+					                   dropdownCallback={deviceInfoChartCallback}
+					                   enableTimeRange
+					                   timeCallback={timeRangeCallback}
+					                   dropdownData={localState.devicesDataLoaded ?
+						                   {
+							                   name: "Devices",
+							                   data: localState.devicesData.map(item => {
+								                   return {
+									                   label: item.deviceId,
+									                   value: item.id
+								                   };
+							                   })
+						                   } : null}
+					/> : (
+						<Grid
+							className={classes.loader}
+							container
+							spacing={0}
+							alignItems="center"
+							justify="center"
+						>
+							<CircularProgress color="secondary" disableShrink/>
+						</Grid>
+					)}
+
+			</div>
+			<button onClick={() => {
+				onClick()
+			}}>Click
+			</button>
+			<Notifications type={"success"} place={"br"} ref={notif}/>
+		</>
+	);
 }
