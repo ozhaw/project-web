0e453c053d7b83c25b402e28c57917702844ad99
ozhaw <54807759+ozhaw@users.noreply.github.com>
2019-11-17 20:19:23 +0200

    Added table data export support

--- webapp/src/components/Table.jsx
+++ webapp/src/components/Table.jsx
@@ -1,46 +1,84 @@
 import React from "react";
 
-import {Card, CardBody, CardHeader, CardTitle, Table} from "reactstrap";
+import {Card, CardBody, CardHeader, CardTitle, Col, Row, Table} from "reactstrap";
 import {makeStyles} from "@material-ui/core";
+import {createRESTUrl} from "../variables/general";
+import {saveAs} from "file-saver";
 
 const useStyles = makeStyles(theme => ({
-	thead: {
-		color: '#2c2c2c',
-		fontWeight: '600'
+    thead: {
+        color: '#2c2c2c',
+        fontWeight: '600'
+    },
+	download: {
+    	cursor: 'pointer',
+		marginTop: '20px'
 	}
 }));
 
 export default function SimpleTable(props) {
-	const classes = useStyles();
+    const classes = useStyles();
 
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle tag="h4">{props.title}</CardTitle>
-			</CardHeader>
-			<CardBody>
-				<Table responsive>
-					<thead className={classes.thead}>
-						{props.headers ?
-							(
-								<tr>
-									{props.headers.map(value => <td>{value}</td>)}
-								</tr>
-							) : null
-						}
-					</thead>
-					<tbody>
-						{props.data ?
-							(
-								props.data.map(value => <tr>
-									{value.map(subvalue => <td>{subvalue}</td>)}
-								</tr>)
-							) : null
-						}
-					</tbody>
-				</Table>
-			</CardBody>
-		</Card>
+    const exportData = () => {
+        let name = "report.csv";
+        fetch(createRESTUrl(`/reports/api/reports/export/csv`), {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${sessionStorage.getItem("sessionToken")}`,
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                data: props.data,
+                headers: props.headers,
+                title: props.title
+            })
+        }).then((response) => {
+            if (response.headers.get("FileName")) {
+                name = response.headers.get("FileName");
+            }
+            return response.blob()
+        }).then((blob) => saveAs(blob, name));
+    };
 
-	);
+    return (
+        <Card>
+            <CardHeader>
+				<Row>
+					<Col>
+						<CardTitle tag="h4">{props.title}</CardTitle>
+					</Col>
+					<Col>
+						<div className={classes.download}>
+							<div className="stats" onClick={exportData}>
+								<i className="fa fa-download"/> Download CSV
+							</div>
+						</div>
+					</Col>
+				</Row>
+            </CardHeader>
+            <CardBody>
+                <Table responsive>
+                    <thead className={classes.thead}>
+                    {props.headers ?
+                        (
+                            <tr>
+                                {props.headers.map(value => <td>{value}</td>)}
+                            </tr>
+                        ) : null
+                    }
+                    </thead>
+                    <tbody>
+                    {props.data ?
+                        (
+                            props.data.map(value => <tr>
+                                {value.map(subvalue => <td>{subvalue}</td>)}
+                            </tr>)
+                        ) : null
+                    }
+                    </tbody>
+                </Table>
+            </CardBody>
+        </Card>
+
+    );
 }
